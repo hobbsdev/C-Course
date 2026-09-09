80116f2d497f2f1a5f2f9dbbd6132b8fc2370419
hobbsdev <117094512+hobbsdev@users.noreply.github.com>
2026-09-09 13:07:00 +0000

    Lesson 1

---
+++ chapterOne.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+/* int main() {
+    std::cout << "My Name is " << "Brian" << " and I'm awesome" << " and super cool" << std::endl;
+    std::cout << "Line one" << std::endl;
+    std::cout << "Line two\n" << std::endl;
+    return 0;
+}
+    */
+
+/* int main() {
+    std::cout << " Brian Hobbs " << std::endl;
+    return 0;
+} */
+
+/*
+
+int main() {
+    std::cout << "Pizza" << std::endl;
+    std::cout << "Purple" << std::endl;
+    std::cout << "Boxers" << std::endl;
+
+    std::cout << "2 + 2 " << std::endl;
+    std::cout << 2 + 2 << std::endl;
+    std::cout << "45" << std::endl;
+    std::cout << 45 + 1 << std::endl;
+    return 0;
+}
+    */
+
+
+int main() {
+    std::cout << "=======================\n"; 
+    std::cout << "        About Me       \n"; 
+    std::cout << "=======================\n";
+    std::cout << " Name:   Jordan\n";
+    std::cout << " Age:    12 \n";
+    std::cout << " Loves:  skateboarding\n";
+    std::cout << "========================\n";
+    return 0;
+}
